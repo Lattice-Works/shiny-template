0c253560439ee5c83fd2010066c8f3a5e25262e8
jokedurnez <joke.durnez@gmail.com>
2019-03-28 18:01:51 -0700

    working version of cookies

--- ui.R
+++ ui.R
@@ -1,44 +1,68 @@
-library(shiny)
+source("global.R")
 
 # Define UI for application that draws a histogram
-shinyUI(
-  fluidPage(theme = "custom.css",
-            title ="Cafe",
-    tags$head(
-      tags$link(rel= "stylesheet", type = "text/css", href = "http://fonts.googleapis.com/css?family=Open+Sans"),
-      tags$link(rel = "icon", type = "image/png", href="favicon.png")
-    ),
+    tagList(
+    useShinyjs(),
     navbarPage(
-      title = div(
-        column(img(src="logomark.png", class="logo"),width=1),
-        column(class="portalname", "Cafe", width=6)
-        ),
-      collapsible = FALSE,
-      tabPanel(
-        "first",
-        sidebarLayout(
-          sidebarPanel(
-            sliderInput("vals",
-                        "Number of values:",
-                        min = 10,
-                        max = 100,
-                        value = 30)
+        id = "navbar",
+        title =
+            div(
+                img(src = "logomark.png",
+                    class = "logo"),
+                "My application",
+                class = "navbar-title"
             ),
-          
-            # Show a plot of the generated distribution
-            mainPanel(
-              plotOutput(outputId = "histogram")
-            )
-          )
+        theme = "custom.css",
+        fluid = TRUE,
+        windowTitle = "title",
+        collapsible = TRUE,
+        header = tags$head(
+            tags$style(HTML(
+                "#page-nav > li:first-child { display: none; }"
+            )),
+            tags$link(rel = "stylesheet", type = "text/css", href = "https://fonts.googleapis.com/css?family=Chivo"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "shinydashboard.css"),
+            tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
+            tags$link(rel = "icon", type = "image/png", href = "favicon.png"),
+            tags$script(src = "https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js")
         ),
-      tabPanel(
-        "second",
-        mainPanel(
-          "This is another page !"
-        )
-      )
-      )
-    )
-  )
+        navbarMenu("Figures",
+                   tabPanel("Histogram",
+                            fluidRow(
+                                column(
+                                    width = 4,
+                                    sliderInput(
+                                        "vals",
+                                        "Number of values:",
+                                        min = 10,
+                                        max = 100,
+                                        value = 30
+                                    )
+                                ),
+                                column(
+                                    width = 8,
+                                    box(
+                                        width = 12,
+                                        solidHeader = TRUE,
+                                        title = "Histogram",
+                                        plotOutput('histogram', height = 500)
+                                        )
+                                    )
+                                )
+                            )
+        ),
+        navbarMenu("Tables",
+                   tabPanel("Demo data",
+                            fluidRow(
+                                box(
+                                    width = 12,
+                                    solidHeader = TRUE,
+                                    title = "Demo data",
+                                    dataTableOutput(outputId = "demo_table")
+                                    )
+                                )
+                            )
 
- 
+        )
+    )
+)
